9ee8f4c7ece62ab6447c5d75eb1e22baaee996fe
Jose A. Herrero <j.herrero.moriana@gmail.com>
2018-02-28 12:54:30 +0100

    Extract mail service

---
+++ mail/mailConfig.dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+    credentials: {
+        user: 'someone@gmail.com',
+        pass: '*****'
+    },
+    mail: {
+        from: 'someone@gmail.com',
+        to: 'someone@gmail.com',
+        subject: 'Chiste del día'
+    }
+}
