34a3935ed38f6bc4046e38682c9c5c2281f41651
jose.antonio.herrero.moriana <jose.antonio.herrero.moriana@ing.com>
2023-04-01 18:01:22 +0200

    fix dependencies

--- services/jokeService.js
+++ services/jokeService.js
@@ -1,15 +1,14 @@
-var request = require('request');
+const https = require('https');
 
 const SERVICE_URL = 'https://icanhazdadjoke.com';
 
 function jokeService() {
     this.getJoke = function() {
-        return new Promise(function(resolve, reject) {
-            request(SERVICE_URL, { json: true }, (err, res, body) => {
-                if (err) { reject(err); }
-                resolve(res.body.joke);
-            });
-        });
+        return https.get(SERVICE_URL, res => {
+            res.on('data', chunk => {
+                return chunk
+            })
+        })
     };
 };
 
